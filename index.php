929300fac2c0d1fc3b4a194a2928768e305a5058
Victor Malai <victor.malai@7code.ro>
2019-10-30 16:43:08 +0100

    DB

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 
-global $wp_rewrite;
-$wp_rewrite->flush_rules();
-
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
